df5212db8f7381651f0c2d7041b66c0841ae74fb
Fauzan <fauzanlk@gmail.com>
2015-09-27 10:49:28 +0700

    add codename.php and

--- README.md
+++
@@ -1,2 +0,0 @@
-# jsystem
-Hey fauzan
--- module/setting/teacher/function/codeName.php
+++ module/setting/teacher/function/codeName.php
@@ -1,15 +1,5 @@
 <?php 
 	function codeName(){
-		$t_codeName = mysqli_query($con, "SELECT max(t_code) AS mx FROM teachers");
-		$row = mysqli_fetch_assoc($t_codeName);
-		$mem_old = $row['mx'];// gives the highest id
-
-		$tmp1=substr($mem_old,1);
-		$tmp2=$tmp1+1;
-
-		if($tmp2 <= 9){
-		    $tmp3='000'.$tmp2;
-		}
-
-		$a = "Fauzan";
+		$m = "Fauzan";
+	}
 ?>
